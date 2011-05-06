1dcd670178b4e5f57881f93d21fc8b9ea44d3ada
erep <erep@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-06 22:23:25 +0000

    TER upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/directmail_return_scheduler/trunk@47389 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "directmail_return_scheduler".
 #
-# Auto generated 02-02-2011 21:48
+# Auto generated 07-05-2011 00:22
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.1.dev',
+	'version' => '0.1.2dev',
 	'constraints' => array(
 		'depends' => array(
 			'direct_mail' => '',
@@ -39,7 +39,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"0c1f";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"ffa6";s:20:"doc/wizard_form.html";s:4:"9d86";}',
+	'_md5_values_when_last_written' => 'a:12:{s:9:"ChangeLog";s:4:"0c1f";s:10:"README.txt";s:4:"ee2d";s:16:"ext_autoload.php";s:4:"8bbd";s:12:"ext_icon.gif";s:4:"e22b";s:17:"ext_localconf.php";s:4:"f57f";s:14:"ext_tables.php";s:4:"e7f1";s:13:"locallang.xml";s:4:"7552";s:17:"locallang_csh.xml";s:4:"d795";s:19:"doc/wizard_form.dat";s:4:"ffa6";s:20:"doc/wizard_form.html";s:4:"9d86";s:59:"tasks/class.tx_directmailreturnscheduler_returnanalysis.php";s:4:"1ea2";s:83:"tasks/class.tx_directmailreturnscheduler_returnanalysis_additionalfieldprovider.php";s:4:"5e8a";}',
+	'suggests' => array(
+	),
 );
 
 ?>
